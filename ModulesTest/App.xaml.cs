68aeda7419e14abcc4fda6fee38a8fc7682aabe6
Moisej Braver <moiseibraver@gmail.com>
2020-09-28 20:55:51 +0200

    Add project files.

---
+++ ModulesTest/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ModuleA;
+using ModuleB;
+using ModulesTest.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace ModulesTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            var moduleAType = typeof(ModuleAModule);
+            moduleCatalog.AddModule(new ModuleInfo()
+            {
+                ModuleName = moduleAType.Name,
+                ModuleType = moduleAType.AssemblyQualifiedName,
+                InitializationMode = InitializationMode.WhenAvailable
+            });
+
+            var moduleBType = typeof(ModuleBModule);
+            moduleCatalog.AddModule(new ModuleInfo()
+            {
+                ModuleName = moduleBType.Name,
+                ModuleType = moduleBType.AssemblyQualifiedName,
+                InitializationMode = InitializationMode.WhenAvailable
+            });
+
+        }
+    }
+}
---
+++ ModulesTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace ModulesTest.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string fieldName;
+        public string Module
+        {
+            get { return fieldName; }
+            set { SetProperty(ref fieldName, value); }
+        }
+
+        private DelegateCommand<string> _showModuleCommand;
+        public DelegateCommand<string> ShowModuleCommand =>
+            _showModuleCommand ?? (_showModuleCommand = new DelegateCommand<string>(ExecuteShowModuleCommand));
+
+        void ExecuteShowModuleCommand(string module)
+        {
+            Module = "Module" + module;
+        }
+
+        public MainWindowViewModel()
+        {
+            Module = "ModuleA";
+        }
+    }
+}
